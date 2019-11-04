f0c10d44b7ca74b7ff16cebbddf73b9dd8d42b01
Vik Akam <vikramakam@hotmail.com>
2019-11-03 22:58:59 -0800

    function 2 done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function sum(a, b) { //eslint-disable-line
   var myArray = [];
   var sumAnswer = a + b;
   myArray[0] = sumAnswer;
-  var result = `The sum of ${a} and ${b} is ${sumAnswer}.`
+  var result = `The sum of ${a} and ${b} is ${sumAnswer}.`;
   myArray[1] = result;
   return myArray;
 }
@@ -33,11 +33,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var funcTwoArray = [];
+  var mulAnswer = a * b;
+  funcTwoArray[0] = mulAnswer;
+  var result = `The product of ${a} and ${b} is ${mulAnswer}.`;
+  funcTwoArray[1] = result;
+  return funcTwoArray;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
